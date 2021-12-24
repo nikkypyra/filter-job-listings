818700eca66e18185ec068d06b5e8ab62f1ae978
Nicole Pyra <nikkypyra@outlook.com>
2021-12-24 15:39:07 +0100

    feat: add jobs data

---
+++ src/data/jobs.types.ts
@@ -0,0 +1,57 @@
+export enum Role {
+    Frontend = 'Frontend',
+    Fullstack = 'Fullstack',
+    Backend = 'Backend'
+  }
+  
+export enum Level {
+    Junior = 'Junior',
+    Intermediate = 'Intermediate',
+    Senior = 'Senior'
+  }
+  
+export enum Contract {
+    FullTime = 'Full Time',
+    PartTime = 'Part Time',
+    Contract = 'Contract',
+  }
+  
+export enum Location {
+    Remote = 'Remote',
+    Worldwide = 'Worldwide',
+    Switzerland = 'Switzerland Only'
+  }
+  
+export enum Language {
+    HTML = 'HTML',
+    CSS = 'CSS',
+    JavaScript = 'JavaScript',
+    Python = 'Python',
+    Ruby = 'Ruby'
+  }
+  
+export enum Tool {
+    React = 'React',
+    Vue = 'Vue',
+    Sass = 'Sass',
+    Rails = 'Rails',
+    Django = 'Django'
+  }
+  
+export interface Job {
+    id: number,
+    company: string,
+    logo: string,
+    new: boolean,
+    featured: boolean,
+    position: string,
+    role: Role,
+    level: Level,
+    postedAt: string,
+    contract: Contract,
+    location: Location,
+    languages: Language[],
+    tools: Tool[]
+  }
+  
+export type Jobs = Job[] 
